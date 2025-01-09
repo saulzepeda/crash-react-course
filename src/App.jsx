b7f49c83de2ddbea5c4eba5a903b68ee27201d24
Saúl Eduardo Zepeda de la Torre <saulzepeda@Sauls-MacBook-Pro.local>
2025-01-08 19:25:47 -0600

    AddJob page and query call

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,51 @@
-import { Route, createBrowserRouter, createRoutesFromElements, RouterProvider } from 'react-router-dom'
+import {
+	Route,
+	createBrowserRouter,
+	createRoutesFromElements,
+	RouterProvider,
+} from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import MainLayout from './layouts/MainLayout';
 import JobsPage from './pages/JobsPage';
 import NotFoundPage from './pages/NotFoundPage';
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path='/' element={ <MainLayout /> }>
-      <Route index element={ <HomePage /> } />
-      <Route path='/jobs' element={ <JobsPage /> } />
-      <Route path='*' element={ <NotFoundPage /> } />
-    </Route>
-  )
-);
+import JobPage, { jobLoader } from './pages/JobPage';
+import AddJobPage from './pages/AddJobPage';
 
 function App() {
-  return <RouterProvider router={router} />;
-};
+	// Add New Job
+	const addJob = async (newJob) => {
+		const res = await fetch('/api/jobs', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(newJob),
+		});
+		return;
+	};
 
-export default App
+	// Delete Job
+	const deleteJob = async (id) => {
+		console.log('delete', id);
+	};
+
+	const router = createBrowserRouter(
+		createRoutesFromElements(
+			<Route path="/" element={<MainLayout />}>
+				<Route index element={<HomePage />} />
+				<Route path="/jobs" element={<JobsPage />} />
+				<Route path="add-job" element={<AddJobPage addJobSubmit={addJob} />} />
+				<Route
+					path="/jobs/:id"
+					element={<JobPage deleteJob={deleteJob} />}
+					loader={jobLoader}
+				/>
+				<Route path="*" element={<NotFoundPage />} />
+			</Route>
+		)
+	);
+
+	return <RouterProvider router={router} />;
+}
+
+export default App;
