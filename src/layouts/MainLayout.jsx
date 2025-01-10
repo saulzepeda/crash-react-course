19296cb6c307910c9e11dc6ec9bb632cefa8559f
Saúl Eduardo Zepeda de la Torre <saulzepeda@Sauls-MacBook-Pro.local>
2025-01-10 14:59:02 -0600

    adding toastify for messages

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,12 +1,15 @@
-import { Outlet } from "react-router-dom";
+import { Outlet } from 'react-router-dom';
 import NavBar from '../components/Navbar';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const MainLayout = () => {
-  return (
-    <>
-        <NavBar />
-        <Outlet />
-    </>
-  )
-}
+	return (
+		<>
+			<NavBar />
+			<Outlet />
+			<ToastContainer />
+		</>
+	);
+};
 export default MainLayout;
